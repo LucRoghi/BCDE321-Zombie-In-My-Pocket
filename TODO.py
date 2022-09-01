a74bf873f4ae6562df8e2b2f418211ef22e370ee
Birphon <jaredireland1205@gmail.com>
2022-09-02 11:43:46 +1200

    TODO.py has been updated with the list of GameStates and what they mean

--- TODO.py
+++ TODO.py
@@ -1,3 +1,13 @@
 # TODO:
-#   GameStates - START, MOVING, ROTATE, ZOMBIES, 
+#   GameStates - START, MOVING, ROTATE, DEAD, END, WIN
 #   Pretty Print - Health or what DevCards are played
+
+
+# TODO:
+#   Explaining the current list of GameStates:
+#   START - This only happens when the game is started (player has to run START in the 'mainmenu')
+#   PLAY - This means that the game is playing
+#   ROTATE - This means the player can rotate a card - idk if needed
+#   DEAD - Player has reached 0 HP
+#   END - The Timer is UP
+#   WIN - The Player has buried the totem in the Graveyard
