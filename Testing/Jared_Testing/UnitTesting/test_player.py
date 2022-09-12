3ae55fdfa6e33f39acd4e0d0fac94faba4db5663
Birphon <61985658+Birphon@users.noreply.github.com>
2022-09-12 22:53:06 +1200

    I was dumb. Popping works :)

--- Testing/Jared_Testing/UnitTesting/test_player.py
+++ Testing/Jared_Testing/UnitTesting/test_player.py
@@ -7,8 +7,9 @@ class TestPlayer(unittest.TestCase):
     def test_drop_item(self):
         p = Player()
         p.inventory = ["A", "B"]
-        inv_len = len(p.inventory)
-        print(f"Player's currently holds {inv_len} item's")
+        print(f"Player's currently holds {len(p.inventory)} item's")
+        print("Testing Running...")
         p.drop_item(1)
-        self.assertIs(inv_len, 1, "Passed")
+        self.assertIs(len(p.inventory), 1, "Passed")
+        print(f"Player's currently holds {len(p.inventory)} item's")
 
