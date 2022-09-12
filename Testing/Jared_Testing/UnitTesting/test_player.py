69967d58eed0f95483c575e0c35f954d959564ac
Birphon <61985658+Birphon@users.noreply.github.com>
2022-09-12 23:10:31 +1200

    Cant set Zombine Number to a number due to current_location being a "None" Type in Player :L - Unsure how to test the movement as I don't know how to make a current location to which the player can then move to a "new" location.

--- Testing/Jared_Testing/UnitTesting/test_player.py
+++ Testing/Jared_Testing/UnitTesting/test_player.py
@@ -3,13 +3,26 @@ from Model.player import Player
 
 
 class TestPlayer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_player = Player("Test")
 
     def test_drop_item(self):
         p = Player()
         p.inventory = ["A", "B"]
-        print(f"Player's currently holds {len(p.inventory)} item's")
-        print("Testing Running...")
         p.drop_item(1)
         self.assertIs(len(p.inventory), 1, "Passed")
-        print(f"Player's currently holds {len(p.inventory)} item's")
 
+    def test_flee(self):
+        p = Player()
+        p.current_location.zombie_number = 5
+        self.assertIs(p.flee(), 1, "Passed")
+
+    def test_attack(self):
+        p = Player()
+        p.current_location.zombie_number = 5
+        self.assertIs(p.player_attack(), 2, "Passed")
+
+    def test_cower(self):
+        p = Player()
+        p.current_location.zombie_number = 5
+        self.assertIs(p.cower(), 9, "Passed")
