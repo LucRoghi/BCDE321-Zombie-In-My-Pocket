e68061adb007c334c34320cc0f61cc4e67bf108d
Birphon <61985658+Birphon@users.noreply.github.com>
2022-10-19 17:46:14 +1300

    Fucking Github I Swear To God
    PAIN

--- Model/dev_card.py
+++ Model/dev_card.py
@@ -7,7 +7,7 @@ Simple Development card set up for the rest of the game
 
 class DevCard:
     """
-    >>> from Model.dev_card import DevCard
+    >>> from ZombiesInMyPocket.Model
     >>> devcard = DevCard("Machete", "Unlimited", "Zombies", "Health", "Item")
     >>> devcard.get_item()
     'Machete'
@@ -29,7 +29,6 @@ class DevCard:
     >>> devcard_two.get_charges()
     1
     """
-
     def __init__(self, item, charges, event_one, event_two, event_three):
         self.item = item
         self.charges = charges
@@ -54,8 +53,3 @@ class DevCard:
     def get_charges(self):
         return self.charges
 
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
